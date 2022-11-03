1765e07c4985fbc3123d33253ed168efa81182b5
Joe Gaffey <642629+joegaffey@users.noreply.github.com>
2022-11-03 00:11:46 +0000

    Update VHRC.ino

--- VHRC.ino
+++ VHRC.ino
@@ -4,12 +4,7 @@
 
 AS5600 as5600;   //  use default Wire
 
-//Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
-//  JOYSTICK_TYPE_MULTI_AXIS, 1, 0,
-//  false, false, false, false, false, false,
-//  false, false, true, true, true);
-
-  Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
   JOYSTICK_TYPE_GAMEPAD, 1, 0,
   true, true, true, false, false, false,
   false, false, false, false, false);
@@ -30,10 +25,6 @@ void setup()
   as5600.setDirection(AS5600_CLOCK_WISE);  // default, just be explicit.
   
   Joystick.begin();
-//  Joystick.setSteeringRange(80, 280);
-//  Joystick.setAcceleratorRange(20, 520);
-//  Joystick.setBrakeRange(540, 1020);
-
   Joystick.setXAxisRange(80, 280);
   Joystick.setYAxisRange(20, 520);
   Joystick.setZAxisRange(540, 1020);
@@ -43,26 +34,7 @@ void setup()
 
 void loop()
 {
-//  if(digitalRead(15))
-//    Joystick.pressButton(1);
-    
   int thBr = analogRead(19);
-  
-//  if(thBr > 10 && thBr < 520) {
-//    Joystick.setBrake(0);
-//    Joystick.setAccelerator(520 - thBr);
-//  }
-//  else if(thBr > 540) {
-//    Joystick.setAccelerator(0);
-//    Joystick.setBrake(thBr);
-//  }
-//  else if(thBr >= 520 && thBr <= 540) {
-//    Joystick.setBrake(0);
-//    Joystick.setAccelerator(0);
-//  }
-//  
-//  Joystick.setSteering((as5600.rawAngle() * AS5600_RAW_TO_DEGREES) - center);
-//  Joystick.sendState();
 
   if(thBr > 10 && thBr < 520) {
     Joystick.setZAxis(0);
